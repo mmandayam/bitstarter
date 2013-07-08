4053404e6c2d960515190ef1a88b1d78273edf97
Murali Mandayam <mm94539@yahoo.com>
2013-07-08 10:53:50 +0000

    Hopefully modified web.js properly

--- web.js
+++ web.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-var index_text = new Buffer(50, "ascii");
-var index_text = fs.readFileSync('index.html', function(err,data) {
-		 if (err) then err;
-		 console.log(data);
-});
+var index_text = new Buffer(50);
+var index_text = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
