f71c24a379bcadf2bfc23d32ad463aa21bbf60f3
Murali Mandayam <mm94539@yahoo.com>
2013-07-08 10:38:22 +0000

    Added index.html as a string

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var index_text = new Buffer(50, "ascii");
+var index_text = fs.readFileSync('index.html', function(err,data) {
+		 if (err) then err;
+		 console.log(data);
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World-2!');
+//  response.send('Hello World-2!');
+    response.send(index_text);
 });
 
 var port = process.env.PORT || 5000;
