60eb76bb3ad6d4e6ee5b6f36df043d993f1031c7
Murali Mandayam <mm94539@yahoo.com>
2013-07-08 11:01:20 +0000

    Added ascii as the file option

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var fs = require('fs');
-var index_text = new Buffer(50);
-var index_text = fs.readFileSync('index.html');
-
+var index_text = new Buffer(50, 'ascii');
+var index_text = fs.readFileSync('index.html', 'ascii');
+console.log(index_text);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
